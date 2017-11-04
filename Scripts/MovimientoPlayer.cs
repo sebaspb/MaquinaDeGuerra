29d4000921e64780f6f2b6e16c7ebd56290bb176
sebaspb <32881159+sebaspb@users.noreply.github.com>
2017-11-04 09:27:34 -0500

    Versión 0.1
    Primeras pruebas de scripts para el juego

---
+++ Scripts/MovimientoPlayer.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MovimientoPlayer : MonoBehaviour
+{
+
+    public float speed = 10.0F;
+    public float rotationSpeed = 100.0F;
+    public float thrust = 5.0f;
+    public Rigidbody rb;
+    public int totalsalud;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+        transform.Translate(0, 0, translation);
+        transform.Rotate(0, rotation, 0);
+    }
+
+    void FixedUpdate()
+    {
+      
+        if (Input.GetButtonDown("Jump") && EstaEnElPiso == true)
+        {
+            rb.AddForce(0, thrust, 0, ForceMode.Impulse);
+        }
+    }
+
+    public bool EstaEnElPiso = false;
+
+    void OnCollisionStay(Collision collision)
+    {
+        if (collision.collider.CompareTag("terreno"))
+        {
+
+            EstaEnElPiso = true;
+            Debug.Log("Tocando el piso " + EstaEnElPiso);
+        }
+    }
+
+    void OnCollisionExit(Collision cosito)
+    {
+
+        EstaEnElPiso = false;
+        print("No longer in contact with " + EstaEnElPiso);
+    }
+}
+
+
