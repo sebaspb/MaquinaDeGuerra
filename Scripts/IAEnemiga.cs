31f3d4e55fa1145e5bc736a31d485d7ce45e0dbf
sebaspb <32881159+sebaspb@users.noreply.github.com>
2017-11-04 23:00:17 -0500

    Version 0.3
    Se han mejorado algunos códigos y se ha comenzado a utilizar modelos más avanzados.

---
+++ Scripts/IAEnemiga.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IAEnemiga : MonoBehaviour {
+
+
+    public GameObject[] Enemigos;
+    public float TiempoDeSpawn = 3f;
+    public Transform[] PuntoDeSpawn;
+    public float puntuacion = 50f;
+
+    
+
+    // Use this for initialization
+    void Start () {
+
+        InvokeRepeating("Spawn", TiempoDeSpawn, TiempoDeSpawn);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void Spawn()
+    {
+
+        if (puntuacion < 50)
+        {
+         
+            int spawnPointIndex = Random.Range(0, PuntoDeSpawn.Length);
+
+        Instantiate(Enemigos[0], PuntoDeSpawn[spawnPointIndex].position, PuntoDeSpawn[spawnPointIndex].rotation);
+           
+
+         
+        }
+
+        if (puntuacion < 100 && puntuacion > 50)
+        {
+
+            int spawnPointIndex = Random.Range(0, PuntoDeSpawn.Length);
+
+            Instantiate(Enemigos[Random.Range(0, 2)], PuntoDeSpawn[spawnPointIndex].position, PuntoDeSpawn[spawnPointIndex].rotation);
+
+        }
+
+
+        if (puntuacion > 100)
+        {
+
+            int spawnPointIndex = Random.Range(0, PuntoDeSpawn.Length);
+
+            Instantiate(Enemigos[Random.Range(0, 3)], PuntoDeSpawn[spawnPointIndex].position, PuntoDeSpawn[spawnPointIndex].rotation);
+
+        }
+
+
+    }
+
+
+
+}
+
